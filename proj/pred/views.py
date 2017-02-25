d1e9574347f92a23f2eb3085e492146968e3f31a
Nanjiang Shu <nanjiang.shu@bils.se>
2017-02-25 23:07:08 +0100

    change alpha2 to alpha_2 for pycountry in views.py

--- proj/pred/views.py
+++ proj/pred/views.py
@@ -1825,13 +1825,6 @@ def get_serverstatus(request):#{{{
         except:
             pass
 
-#         country = "N/A"           # this is slow
-#         try:
-#             match = geolite2.lookup(ip)
-#             country = pycountry.countries.get(alpha2=match.country).name
-#             countrylist.append(country)
-#         except:
-#             pass
 
         if method_submission == "web":
             numjob_wed += 1
@@ -1896,7 +1889,7 @@ def get_serverstatus(request):#{{{
         country = "N/A"
         try:
             match = geolite2.lookup(ip)
-            country = pycountry.countries.get(alpha2=match.country).name
+            country = pycountry.countries.get(alpha_2=match.country).name
         except:
             pass
         activeuserli_njob.append([ip, country, njob, nseq])
@@ -1916,7 +1909,7 @@ def get_serverstatus(request):#{{{
         country = "N/A"
         try:
             match = geolite2.lookup(ip)
-            country = pycountry.countries.get(alpha2=match.country).name
+            country = pycountry.countries.get(alpha_2=match.country).name
         except:
             pass
         activeuserli_nseq.append([ip, country, njob, nseq])
